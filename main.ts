a36f6c95a82961a1ff74cd1fe2cfda48eb020755
andrew-yazbeck <andrew.yazbeck@stu.ocsb.ca>
2025-10-27 22:52:26 -0400

    shows nested loops

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Andrew
+ * Created on: Oct 2025
+ * This program shows nested loops
 */
 
-basic.showString('Hello, World!')
+// variables
+let ones: number = 0
+let tens: number = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// when "A" is pressed, the pixels move down in a diagnol
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+
+    tens = 0
+    while (tens <= 9) {
+        ones = 0
+        while (ones <= 9) {
+            basic.showString(tens.toString() + ones.toString())
+            ones++
+        }
+        tens++
+    }
+
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+})
